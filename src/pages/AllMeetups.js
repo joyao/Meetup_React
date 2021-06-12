ff3bd36c308ea69fa02e4c9944269699e2746391
JYHsu <hsujoyao@gmail.com>
2021-06-12 10:22:58 +0800

    [fix] fix to use online db data to show all meetup page items

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const AllMeetupsPage = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    fetch("https://react-app-22a28-default-rtdb.firebaseio.com/meetups.json")
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            setIsLoading(false);
-            setLoadedMeetups(data);
-        });
+    useEffect(() => {
+        setIsLoading(true);
+        fetch("https://react-app-22a28-default-rtdb.firebaseio.com/meetups.json")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                const meetups = [];
+                for (const key in data) {
+                    const meetup = {
+                        id: key,
+                        ...data[key],
+                    };
+                    meetups.push(meetup);
+                }
+                setIsLoading(false);
+                setLoadedMeetups(meetups);
+            });
+    }, []);
 
     if (isLoading) {
         return (
